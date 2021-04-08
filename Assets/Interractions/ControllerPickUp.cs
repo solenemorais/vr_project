8d53ae5cc427b4a45b67876b57a8c677ce814630
solenemorais <solene.morais@esme.fr>
2021-04-08 19:15:48 +0200

    second commit

--- Assets/Interractions/ControllerPickUp.cs
+++ Assets/Interractions/ControllerPickUp.cs
@@ -8,26 +8,30 @@ public class ControllerPickUp : MonoBehaviour
 
     public void PickUp()
     {
+        //stocker collider de la sphère/planete
         Collider[] hitItems = Physics.OverlapSphere(transform.position, 5f);
+
+        //on attache le collider
         pickedUpItem = hitItems[0].attachedRigidbody;
+        //on envoie la position au rigidbody et la fonction pour tenir l'objet
         pickedUpItem.SendMessage("OnItemPickedUp", transform);
     }
 
+    //relâcher l'objet
     public void Release()
     {
         if (pickedUpItem != null )
         {
+
             pickedUpItem.SendMessage("OnItemRelease");
             pickedUpItem = null;
-        }
-        
+        }        
     }
 
     // Update is called once per frame
     void Update()
     {
         //lancer le script pickup à l'appuis sur le bouton de pickup de la manette
-
         if (Input.GetKey(KeyCode.Space))
         {
             PickUp();
@@ -35,7 +39,6 @@ public class ControllerPickUp : MonoBehaviour
 
 
         //lancer le script de release à l'appuis du bouton de release
-
         else
         {
             Release();
--- Assets/Interractions/PickableItem.cs
+++ Assets/Interractions/PickableItem.cs
@@ -4,26 +4,33 @@ using UnityEngine;
 
 public class PickableItem : MonoBehaviour
 {
-
+    //variable pour savoir si l'objet est tenu ou non
     bool isPickedUp;
     private Vector3 startVelocity = new Vector3(5F, 5F, 0F);
 
-
+    //quand on tient l'objet
     public virtual void OnItemPickedUp(Transform holder)
     {
+        //variable passe à vrai
         isPickedUp = true;
+        //desactiver la gravité
         GetComponent<Gravity>().enabled = false;
-        GetComponent<Rigidbody>().velocity = Vector3.zero;
-        GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+        //mettre les vitesses à zéro
+        GetComponent<Rigidbody>().velocity = Vector3.zero; 
+        GetComponent<Rigidbody>().angularVelocity = Vector3.zero; 
+
         transform.SetParent(holder, true);
     }
 
-
+    //quand on relache l'objet
     public virtual void OnItemRelease()
     {
+        //variable passe à faux
         isPickedUp = false;
         transform.SetParent(null, true);
+        //redonner la vitesse à la planete
         GetComponent<Rigidbody>().velocity = startVelocity;
+        //réactiver le script gravité
         GetComponent<Gravity>().enabled = true;
     }
 
